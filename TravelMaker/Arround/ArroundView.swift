137407e3ff478180a81607e95ee1d805e6a2053f
chanho <lchh93@naver.com>
2023-09-11 11:08:56 +0900

    [iOS] TabBar set

---
+++ TravelMaker/Arround/ArroundView.swift
@@ -0,0 +1,29 @@
+//
+//  ArroundView.swift
+//  TravelMaker
+//
+//  Created by 이찬호 on 2023/09/11.
+//
+
+import UIKit
+
+class ArroundView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TravelMaker/Recommend/RecommendView.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendView.swift
+//  TravelMaker
+//
+//  Created by 이찬호 on 2023/09/11.
+//
+
+import UIKit
+
+class RecommendView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
