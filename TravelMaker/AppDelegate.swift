3a232a4fdfa4f20ff533e40154b81e65458eb686
chanho <lchh93@naver.com>
2023-09-13 09:24:21 +0900

    [iOS] 네이버 로그인 셋팅

--- TravelMaker/AppDelegate.swift
+++ TravelMaker/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setNaverLogin()
+        
         return true
     }
 
@@ -30,7 +31,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
 
-
+//MARK: 네이버 로그인 초기화
+extension AppDelegate {
+    func setNaverLogin() {
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        instance?.isNaverAppOauthEnable = true
+        instance?.isInAppOauthEnable = true
+        instance?.isOnlyPortraitSupportedInIphone()
+        instance?.serviceUrlScheme = kServiceAppUrlScheme
+        instance?.consumerKey = kConsumerKey
+        instance?.consumerSecret = kConsumerSecret
+        instance?.appName = kServiceAppName
+    }
 }
 
