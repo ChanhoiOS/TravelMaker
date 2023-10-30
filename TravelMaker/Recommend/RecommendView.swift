6b3798d94b8023d1fe0a1aac3ce0ded091be3efa
chanho <lchh93@naver.com>
2023-10-30 16:55:16 +0900

    [iOS] 추천화면 네이버 지도

--- TravelMaker/Recommend/RecommendView.swift
+++ TravelMaker/Recommend/RecommendView.swift
@@ -6,12 +6,59 @@
 //
 
 import UIKit
+import NMapsMap
+import NMapsGeometry
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+import ReactorKit
+import Kingfisher
 
 class RecommendView: UIViewController {
+    
+    var naverMapView: NMFMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setMap()
+        setCoordinate()
+    }
+}
+
+extension RecommendView: NMFMapViewTouchDelegate {
+    func setCoordinate() {
+        let gpsDisposable = LocationManager.shared.locationSubject
+            .subscribe(onNext: { [weak self] gps in
+                let x = gps?.longitude ?? 127.05485569769449
+                let y = gps?.latitude ?? 37.50518440330725
+                self?.setLocation(x, y)
+            }, onCompleted: {
+                print("onComplete")
+            }) {
+                print("disposed")
+            }
         
+        gpsDisposable.dispose()
+    }
+    
+    func setMap() {
+        naverMapView = NMFMapView()
+            .then {
+                self.view.addSubview($0)
+                
+                $0.touchDelegate = self
+                
+                $0.snp.makeConstraints { make in
+                    make.top.left.right.equalToSuperview()
+                    make.bottom.equalToSuperview()
+                }
+            }
+    }
+    
+    func setLocation(_ x: Double, _ y: Double) {
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: y, lng: x))
+        naverMapView?.moveCamera(cameraUpdate)
     }
 }
