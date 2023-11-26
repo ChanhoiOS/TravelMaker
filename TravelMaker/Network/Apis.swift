40e53c2171445f1134472b9c3770d783314b787f
ChanhoiOS <lchh1301@gmail.com>
2023-11-26 14:01:47 +0900

    [iOS] baseUrl 변경

--- TravelMaker/Network/Apis.swift
+++ TravelMaker/Network/Apis.swift
@@ -8,15 +8,66 @@
 import Foundation
 
 struct Apis {
-    static let baseUrl = "http://43.200.32.96:8140"
+    static let baseUrl = "http://mytravelmaker.shop:3000"
+    
+    
+    
+    static let join = "\(Apis.baseUrl)/api/user/join"
+    
+    static let leave = "\(Apis.baseUrl)/api/user/delete"
+    
+    static let leave_reason = "\(Apis.baseUrl)/api/delete_account"
+    
+    static let nickname_check = "\(Apis.baseUrl)/api/user/nickname/check"
     
     static let login = "\(Apis.baseUrl)/api/auth/login"
     
-    static let postAround = "\(Apis.baseUrl)/api/post"
+    static let update = "\(Apis.baseUrl)/api/user/update"
     
-    static let getAround = "\(Apis.baseUrl)/api/post/all"
+    static let update_image = "\(Apis.baseUrl)/api/user/update/image"
     
-    static let recommendAll = "\(Apis.baseUrl)/api/place/all"
     
-    static let imageUrl = "\(Apis.baseUrl)/images/"
+    
+    static let recommend_all = "\(Apis.baseUrl)/api/recommend/all"
+    
+    static let recommend_one = "\(Apis.baseUrl)/api/recommend"
+    
+    static let bookmark_add = "\(Apis.baseUrl)/api/recommend_bookmark/add"
+    
+    static let bookmark_recommend = "\(Apis.baseUrl)/api/recommend_bookmark"
+    
+    static let bookmark_delete = "\(Apis.baseUrl)/api/recommend_bookmark"
+    
+    
+    
+    static let nearby_add = "\(Apis.baseUrl)/api/nearby/post"
+    
+    static let nearby_all = "\(Apis.baseUrl)/api/nearby/all"
+    
+    static let nearby_one = "\(Apis.baseUrl)/api/nearby"
+    
+    static let nearby_add_bookmark = "\(Apis.baseUrl)/api/nearby_bookmark/add"
+    
+    static let nearby_bookmakr_all = "\(Apis.baseUrl)/api/nearby_bookmark"
+    
+    static let nearby_delete = "\(Apis.baseUrl)/api/nearby_bookmark"
+    
+    
+    
+    static let route_add = "\(Apis.baseUrl)/api/route/add"
+    
+    static let route_all = "\(Apis.baseUrl)/api/route"
+    
+    static let route_one = "\(Apis.baseUrl)/api/route"
+    
+    static let route_delete = "\(Apis.baseUrl)/api/route/delete"
+    
+    static let route_size = "\(Apis.baseUrl)/api/route/bookmark/size"
+    
+    
+    
+    
+    static let dowonload_image = "\(Apis.baseUrl)/api/file/download/image/MTY5NzQyOTI0NjgyNw==.jpeg"
+    
+    
 }
