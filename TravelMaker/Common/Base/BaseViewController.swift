857718f60716794947f37b834a202fbee2b23f3a
chanho <lchh93@naver.com>
2023-12-12 17:51:10 +0900

    [iOS] 백 제스쳐 추가

--- TravelMaker/Common/Base/BaseViewController.swift
+++ TravelMaker/Common/Base/BaseViewController.swift
@@ -12,9 +12,14 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.configureSwipeBackGesture()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
+    }
+    
+    func configureSwipeBackGesture() {
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
     }
 }
